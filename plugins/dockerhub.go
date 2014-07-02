d6bfb497f2837c0135a172a0c6e60ba0c05d0925
Evan Hazlett <ejhazlett@gmail.com>
2014-07-01 20:29:18 -0400

    formatting for dockerhub

--- plugins/dockerhub.go
+++ plugins/dockerhub.go
@@ -70,7 +70,11 @@ func (plugin DockerHubPlugin) Handle(message *phoenix.Message) (string, error) {
 		res = res[:24]
 	}
 	for _, r := range res {
-		data += fmt.Sprintf("> %s: %s\n", r.Name, r.Description)
+		data += fmt.Sprintf("> %s", r.Name)
+		if r.Description != "" {
+			data += fmt.Sprintf(": %s", r.Description)
+		}
+		data += "\n"
 	}
 	return data, nil
 }
